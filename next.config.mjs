8f8798c4b66491baf783d05d753cfd9ed796c774
Paweł Bolek <pawellekb@gmail.com>
2024-10-12 01:06:16 +0200

    Add sass

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+import { fileURLToPath } from 'url';
+
+const nextConfig = {
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+};
 
 export default nextConfig;
